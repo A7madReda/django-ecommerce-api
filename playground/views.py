ecdfccbfe59c574bf2219e16b5d8d976a9de7a70
A7madReda <95848196+A7madReda@users.noreply.github.com>
2022-10-01 12:55:05 +0200

    Sending Emails

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,14 @@
+from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
-
+from templated_mail.mail import BaseEmailMessage
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    try:
+      message = BaseEmailMessage(
+        template_name='emails/hello.html',
+        context={'name': 'Ahmed'}
+      )
+      message.send(['admin@ahmedredabuy.com'])
+    except BadHeaderError:
+       pass
+    return render(request, 'hello.html', {'name': 'Ahmed'})
