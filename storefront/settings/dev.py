0d2da17b5e9dd6247b7b447d205e4186646a4dc9
A7madReda <95848196+A7madReda@users.noreply.github.com>
2022-10-03 00:12:19 +0200

    hide info

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -2,14 +2,14 @@ from storefront.settings.common import *
 
 DEBUG = True
 
-SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
+SECRET_KEY = ''
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': '12345'
+        'ENGINE': '',
+        'NAME': '',
+        'HOST': '',
+        'USER': '',
+        'PASSWORD': ''
     }
 }
